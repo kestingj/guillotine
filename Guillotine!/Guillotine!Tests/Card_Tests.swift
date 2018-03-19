fb829d3830e13906e79f22b82f5e0cf2cfed5ddf
Joseph Kesting <kestingjoseph@gmail.com>
2018-03-19 21:23:48 +0000

    Added Card Tests

--- Guillotine!/Guillotine!Tests/Card_Tests.swift
+++ Guillotine!/Guillotine!Tests/Card_Tests.swift
@@ -6,14 +6,36 @@ import XCTest
 class Card_Tests: XCTestCase {
     
     func testDescription() {
+        let aceOfDiamonds = Card(rank: 14, suit: Suit.DIAMOND)
+        assert(aceOfDiamonds.description == "ace_of_diamonds")
         
+        let kingOfClubs = Card(rank: 13, suit: Suit.CLUB)
+        assert(kingOfClubs.description == "king_of_clubs")
+        
+        let queenOfHearts = Card(rank: 12, suit: Suit.HEART)
+        assert(queenOfHearts.description == "queen_of_hearts")
+        
+        let jackOfSpades = Card(rank: 11, suit: Suit.SPADE)
+        assert(jackOfSpades.description == "jack_of_spades")
+        
+        let twoOfSpades = Card(rank: 2, suit: Suit.SPADE)
+        assert(twoOfSpades.description == "2_of_spades")
     }
     
-    func testGreaterThan() {
+    func testSuitComparison() {
+        let lesserSuit = Card(rank: 2, suit: Suit.SPADE)
+        let greaterSuit = Card(rank: 2, suit: Suit.CLUB)
         
+        assert(lesserSuit < greaterSuit)
+        assert(greaterSuit > lesserSuit)
     }
     
-    func testLesserThan() {
+    func testRankComparison() {
+        let lesserRank = Card(rank: 10, suit: Suit.SPADE)
+        let greaterRank = Card(rank: 11, suit: Suit.SPADE)
+        
+        assert(lesserRank < greaterRank)
+        assert(greaterRank > lesserRank)
         
     }
 }
