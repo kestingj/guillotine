2365d7e303f8ebf22e3a4358119e4e35d53437a3
Joseph Kesting <kestingjoseph@gmail.com>
2018-09-06 23:00:05 -0700

    Finish out LoadBalancer. TODO: hook in results of LoadBalancer to GamesViewController

--- Guillotine!/Guillotine!/Backend/LoadBalancer.swift
+++ Guillotine!/Guillotine!/Backend/LoadBalancer.swift
@@ -23,15 +23,41 @@ class LoadBalancer {
         
         let request = Alamofire.request(url, method: .get)
         
-        var games: [GameMetadata]
+        var games: [GameMetadata] = []
         
         request.responseJSON { responseData in
             if let result = responseData.result.value {
                 let JSON = result as! NSDictionary
-                games = JSON.object(forKey: "games") as! [GameMetadata]
+                let gameArray = JSON.object(forKey: "games") as! [NSDictionary]
+                for game in gameArray {
+                    let gameId = game.object(forKey: "gameId") as! String
+                    let playerIds = game.object(forKey: "playerIds") as! [String]
+                    let hostName = game.object(forKey: "hostName") as! String
+                    
+                    games.append(GameMetadata(gameId: gameId, hostName: hostName, playerIds: playerIds))
+                }
             }
         }
         
         return games
+    }
+    
+    func startNewGame(playerIds: [String]) -> GameMetadata {
+        let url = urlBase + "/games"
+        let parameters: [String: AnyObject] = ["playerIds": playerIds as AnyObject]
+        let request = Alamofire.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+        
+        var game: GameMetadata
+        
+        request.responseJSON { responseData in
+            if let result = responseData.result.value {
+                let JSON = result as! NSDictionary
+                let gameId = JSON.object(forKey: "gameId") as! String
+                let hostName = JSON.object(forKey: "hostName") as! String
+                game = GameMetadata(gameId: gameId, hostName: hostName, playerIds: playerIds)
+            }
+        }
+        
+        return game
     }
 }
